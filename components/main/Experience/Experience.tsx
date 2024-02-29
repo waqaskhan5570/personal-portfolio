a2f46256a75624a63b32a9f4cc6cbc0b879d2336
Waqas Khan <waqaskhan5570@gmail.com>
2024-02-29 19:48:21 +0500

    Fix spacing in experience

--- components/main/Experience/Experience.tsx
+++ components/main/Experience/Experience.tsx
@@ -5,10 +5,7 @@ import { Experience as ExperienceData } from "@/constants";
 
 const Experience = () => {
   return (
-    <div
-      className="flex flex-col items-center justify-center py-20"
-      id="experience"
-    >
+    <div className="flex flex-col items-center justify-center" id="experience">
       <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20">
         Experience
       </h1>
