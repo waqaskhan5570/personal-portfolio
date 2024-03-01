f7e8ba9d3cadaca0cb28fb33652afcf594e383d0
Waqas Khan <waqaskhan5570@gmail.com>
2024-03-01 19:35:45 +0500

    Update skills

--- components/main/Skills.tsx
+++ components/main/Skills.tsx
@@ -13,18 +13,6 @@ const Skills = () => {
       <SkillText />
 
       <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
-        {Skill_data.slice(5).map((image, index) => (
-          <SkillDataProvider
-            key={index}
-            src={image.Image}
-            width={image.width}
-            height={image.height}
-            index={index}
-          />
-        ))}
-      </div>
-
-      <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
         {Skill_data.slice(0, 5).map((image, index) => (
           <SkillDataProvider
             key={index}
@@ -37,7 +25,7 @@ const Skills = () => {
       </div>
 
       <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
-        {Backend_skill.map((image, index) => (
+        {Skill_data.slice(5).map((image, index) => (
           <SkillDataProvider
             key={index}
             src={image.Image}
@@ -47,6 +35,18 @@ const Skills = () => {
           />
         ))}
       </div>
+
+      {/* <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
+        {Backend_skill.map((image, index) => (
+          <SkillDataProvider
+            key={index}
+            src={image.Image}
+            width={image.width}
+            height={image.height}
+            index={index}
+          />
+        ))}
+      </div> */}
 
       <div className="w-full h-full absolute">
         <div className="w-full h-full z-[-10] opacity-30 absolute flex items-center justify-center bg-cover">
